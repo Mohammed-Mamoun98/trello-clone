248fd42626193f359bbe914064b9169cc062df9d
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2020-10-04 18:46:40 +0300

    clean prev methods of redux

--- src/redux/methods/filters-processing.js
+++
@@ -1,8 +0,0 @@
-export const extractFilters = (filterResponse) => {
-  const allFilters = filterResponse?.filters || []; //INCLUDING TIME INTERVAL FILTERS
-  const reportFilters = allFilters.filter((f) => f.group_id === "WIM Rollup");
-  const timeFilters = allFilters.filter(
-    (f) => f.group_id === "Date Interval Group"
-  );
-  return { reportFilters, timeFilters };
-};
